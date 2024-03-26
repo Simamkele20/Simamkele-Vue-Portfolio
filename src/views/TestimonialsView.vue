c59b1b681266f5ab873528d2c5e834b45aa8ab4c
Simamkele20 <132574441+Simamkele20@users.noreply.github.com>
2024-03-26 15:05:24 +0200

    updates

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container bg-white">
-        <h2 class="text-danger mt-3">Testimonials</h2>
+        <h2 class="hea text-danger mt-3">Testimonials</h2>
     </div>
 
  <div class="row">
     <div class="card mx-5    mb-5 mt-4 " v-for="testimonials in Testimonials" :key="testimonials" id="ProjectCard">
                 <img :src="testimonials.profile" class="home2 rounded-al       mt-4 mx-0" alt="...">
                 <div class="card-body">
-                  <h2 class="card-title text-center text-danger ">{{ testimonials.name }} {{ testimonials.surname }} </h2>
+                  <h2 class="hea card-title text-center text-danger ">{{ testimonials.name }} {{ testimonials.surname }} </h2>
                   <h4 class="card-text text-center">{{ testimonials.quotes }}
                   </h4>
                
